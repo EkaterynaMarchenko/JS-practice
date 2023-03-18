015146ed0afcfe4f63bd68f02fe4f4e3d5240ffc
Kateryna <cosm.marchenko@gmail.com>
2023-03-18 10:58:31 +0000

    Methods and properties

--- js/script.js
+++ js/script.js
@@ -235,4 +235,31 @@ const logger = function() {
 
 logger();
 
+   /* МЕТОДЫ И СВОЙСТВА СТРОК И ЧИСЕЛ */
+
+const str = "test";
+
+console.log(str.length); 
+console.log(str.toUpperCase()); 
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q")); 
+
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11)); 
+
+console.log(logg.substring(6, 11)); 
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
 
